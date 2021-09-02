c39cfb0485904027c23ec233c4a5fc0d2441340d
Adrian Américo <1fnadrian1@gmail.com>
2021-09-01 22:10:05 -0300

    adicionado estrutura HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import React, { useEffect } from 'react';
 import Router from './Router';
-import { loteriasConcurso, concurso } from './requests';
 
 function App() {
-  useEffect(() => {
-    concurso(5534);
-  }, []);
   return (
     <div className="App">
       <Router />
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,16 +1,43 @@
 import React from 'react';
+import Numbers from '../../components/Numbers';
 import styles from './homePage.module.scss';
 
 const HomePage: React.FC = () => {
   return (
     <div className={styles.root}>
-     <div>
+      <div className={styles.leftSide}>
+        <div>
+          <label>
+            <select>
+              <option>Mega-sena</option>
+            </select>
+          </label>
 
-     </div>
-     
-     <div>
+          <span>mega-sena</span>
+        </div>
 
-     </div>
+        <div>
+          <h4>Concurso</h4>
+          <h5>4531 - 07/04/2020</h5>
+        </div>
+      </div>
+
+      <div className={styles.rigthSide}>
+        <div>
+          <Numbers />
+          <Numbers />
+          <Numbers />
+          <Numbers />
+          <Numbers />
+          <Numbers />
+        </div>
+        <div>
+          <span>
+            Este sorteio é meramente ilustrativo e não possui nenhuma ligação
+            com a CAIXA
+          </span>
+        </div>
+      </div>
     </div>
   );
 };

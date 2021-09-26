9df67d9e9f634b8318e54cf206892f32efd875ce
Adrian Américo <1fnadrian1@gmail.com>
2021-09-25 22:07:41 -0300

    inserido material ui e corrigido problemas com UI

--- src/components/Numbers/index.tsx
+++ src/components/Numbers/index.tsx
@@ -1,7 +1,11 @@
 import styles from './numbers.module.scss';
 
 const Numbers: React.FC = (props) => {
-  return <div className={styles.root}>{props.children}</div>;
+  return (
+    <div className={styles.root}>
+      <span>{props.children}</span>
+    </div>
+  );
 };
 
 export default Numbers;
